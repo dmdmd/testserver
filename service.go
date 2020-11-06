035c6f27a7e3a2d858a2cb15b504d081d3c2e35b
Diogo Delgado <diogo@getto.app>
2020-11-06 14:21:34 +0100

    added timers

--- service.go
+++ service.go
@@ -10,47 +10,67 @@ import (
 	"net/http/httputil"
 	"reflect"
 	"sort"
+	"time"
 )
 
 func main() {
 	listenAddr := flag.String("http.addr", ":8080", "http listen address")
 	flag.Parse()
 
-	http.HandleFunc("/numbers", handleNumbers)
+	http.HandleFunc("/numbers", handler)
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
 }
 
-func handleNumbers(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+	// c := make(chan int)
 	param := "u"
 	keys, ok := r.URL.Query()[param]
-	numbersMap := map[int]bool{}
+
 	result := map[string][]int{
 		"numbers": {},
 	}
 
 	if !ok || len(keys[0]) < 1 {
 		log.Printf("Url Param '%v' is missing\n", param)
+		respondToClient(w, start, result)
 		return
 	}
 
+	result["numbers"] = handleRequest(keys)
+
+	respondToClient(w, start, result)
+}
+
+func respondToClient(w http.ResponseWriter, start time.Time, result map[string][]int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	log.Printf("total duration %v\n", time.Now().Sub(start))
+	json.NewEncoder(w).Encode(result)
+}
+
+func handleRequest(keys []string) []int {
+	numbersMap := map[int]bool{}
+	result := []int{}
+
 	for _, url := range keys {
-		numbersMap = mergeInMap(numbersMap, handleURL(url))
+		arr, duration := handleURL(url)
+		log.Printf("URL %v duration: %v and value %v\n", url, duration, arr)
+		numbersMap = mergeInMap(numbersMap, arr)
 	}
 
 	for _, k := range reflect.ValueOf(numbersMap).MapKeys() {
 		val := int(k.Int())
-		result["numbers"] = append(result["numbers"], val)
+		result = append(result, val)
 	}
 
-	sort.Ints(result["numbers"])
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	json.NewEncoder(w).Encode(result)
+	sort.Ints(result)
+	return result
 }
 
-func handleURL(url string) []int {
+func handleURL(url string) ([]int, time.Duration) {
+	start := time.Now()
 	page := map[string][]int{
 		"numbers": {},
 	}
@@ -58,14 +78,14 @@ func handleURL(url string) []int {
 
 	if err != nil {
 		log.Printf("Couldn't request %v: %v\n", url, err)
-		return []int{}
+		return []int{}, time.Now().Sub(start)
 	}
 
 	requestDump, err := httputil.DumpResponse(resp, true)
 
 	if err != nil {
 		log.Printf("Url %v didn't return a valid response: %v\n", url, err)
-		return []int{}
+		return []int{}, time.Now().Sub(start)
 	}
 
 	fmt.Println(string(requestDump))
@@ -78,18 +98,18 @@ func handleURL(url string) []int {
 
 		if err != nil {
 			log.Printf("Url %v return code error %v\n", url, err)
-			return []int{}
+			return []int{}, time.Now().Sub(start)
 		}
 
 		// we unmarshal our byteArray which contains our
 		// jsonFile's content
 		json.Unmarshal(bodyBytes, &page)
 
-		return page["numbers"]
+		return page["numbers"], time.Now().Sub(start)
 	}
 
 	log.Printf("Url %v return code %v", url, resp.StatusCode)
-	return []int{}
+	return []int{}, time.Now().Sub(start)
 
 }
 
